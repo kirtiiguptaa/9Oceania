033eed93d4e7d46dbad0fd56522623df0dadedbe
kirtiiguptaa <kirtigupta9449@gmail.com>
2024-04-07 15:58:31 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,69 @@
+// Loading page
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var loader = document.getElementById("preloader");
+        loader.style.display = "none";
+        document.getElementById('video').style.display = 'block';
+    }, 4000); 
+});
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        var loader = document.getElementById("preloader");
+        loader.style.display = "none";
+    }, 5000); 
+    
+});
+
+// Cursor
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets) {
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 250 + "px"
+    blur.style.top = dets.y - 250 + "px"
+});
+
+// Toggle Button
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleBtn = document.getElementById('toggle-btn');
+    const menuList = document.getElementById('menu-list');
+
+    toggleBtn.addEventListener('click', function() {
+        menuList.classList.toggle('show-menu');
+        if (menuList.classList.contains('show-menu')) {
+            menuList.style.display = "block";
+        } 
+        else {
+            menuList.style.display = "none";
+        }
+    });
+    const menuBtn = document.getElementById("mobile-menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu-list");
+
+    menuBtn.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show-mobile-menu');
+        if (mobileMenu.classList.contains('show-mobile-menu')) {
+            mobileMenu.style.display = "block";
+        }
+        else {
+            mobileMenu.style.display = "none";
+        }
+    })
+});
+
+// Function to start the video after a delay
+function startVideo() {
+    const video = document.getElementById('logo-video');
+
+    video.playbackRate = 0.75;
+
+    video.play()
+        .catch(error => {
+            console.error('Autoplay prevented:', error.message);
+        });
+}
+
+setTimeout(startVideo, 4000);
