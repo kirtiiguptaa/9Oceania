2f4f433b3dc401fd6718581a9fc1e1bd081894f7
kirtiiguptaa <kirtigupta9449@gmail.com>
2024-04-12 20:59:59 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 3000); 
 });
 
+// Loading
 window.addEventListener("load", function() {
     setTimeout(function() {
         var loader = document.getElementById("preloader");
@@ -28,30 +29,58 @@ document.addEventListener("mousemove", function(dets) {
 
 // Toggle Button
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleBtn = document.getElementById('toggle-btn');
-    const menuList = document.getElementById('menu-list');
+  const toggleBtn = document.getElementById('toggle-btn');
+  const menuList = document.getElementById('menu-list');
+  const navClose = document.getElementById('nav-close');
 
-    toggleBtn.addEventListener('click', function() {
-        menuList.classList.toggle('show-menu');
-        if (menuList.classList.contains('show-menu')) {
-            menuList.style.display = "block";
-        } 
-        else {
-            menuList.style.display = "none";
-        }
-    });
+  toggleBtn.addEventListener('click', function() {
+      menuList.classList.toggle('show-menu');
+      if (menuList.classList.contains('show-menu')) {
+          menuList.style.display = "block";
+          navClose.classList.add("nav-close");
+          navClose.style.display = "block";
+          toggleBtn.style.display = "none";
+      }
+  });
+
+  navClose.addEventListener('click', function() {
+    menuList.classList.remove('show-menu');
+    menuList.style.display = "none";
+    navClose.classList.remove("nav-close");
+    navClose.style.display = "none";
+    toggleBtn.classList.add("toggle-btn");
+    toggleBtn.style.display = "block";
+});
+
+
+
     const menuBtn = document.getElementById("mobile-menu-btn");
     const mobileMenu = document.getElementById("mobile-menu-list");
+    const menuClose = document.getElementById("mobile-nav-close");
 
     menuBtn.addEventListener('click', function() {
         mobileMenu.classList.toggle('show-mobile-menu');
         if (mobileMenu.classList.contains('show-mobile-menu')) {
             mobileMenu.style.display = "block";
+            menuClose.classList.add("nav-close");
+            menuClose.style.display = "block";
+            menuBtn.style.display = "none";
         }
         else {
-            mobileMenu.style.display = "none";
-        }
+          mobileMenu.style.display = "none";
+          menuClose.classList.remove("nav-close");
+          menuClose.style.display = "none";
+          menuBtn.style.display = "block";
+      }
     })
+    menuClose.addEventListener('click', function() {
+      mobileMenu.classList.remove('show-menu');
+      mobileMenu.style.display = "none";
+      menuClose.classList.remove("nav-close");
+      menuClose.style.display = "none";
+      menuBtn.classList.add("toggle-btn");
+      menuBtn.style.display = "block";
+  });
 });
 
 // Function to start the video after a delay
@@ -68,7 +97,7 @@ function startVideo() {
 
 setTimeout(startVideo, 4000);
 
-
+// Scroll
 var container = document.getElementById('container');
 
 window.addEventListener('wheel', function(e) {
@@ -80,6 +109,32 @@ window.addEventListener('wheel', function(e) {
   } else {
     container.scrollBy({
       left: -100,
+      behavior: 'smooth'
+    });
+  }
+});
+
+var container = document.getElementById('container');
+
+window.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowRight') {
+    container.scrollBy({
+      left: 100,
+      behavior: 'smooth'
+    });
+  } else if (e.key === 'ArrowLeft') {
+    container.scrollBy({
+      left: -100,
+      behavior: 'smooth'
+    });
+  } else if (e.key === 'ArrowUp') {
+    container.scrollBy({
+      top: 100,
+      behavior: 'smooth'
+    });
+  } else if (e.key === 'ArrowDown') {
+    container.scrollBy({
+      top: -100,
       behavior: 'smooth'
     });
   }
