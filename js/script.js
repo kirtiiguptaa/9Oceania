471945b9a80b5151ca781cbf999f4723ea4e04ac
kirtiiguptaa <kirtigupta9449@gmail.com>
2024-04-16 00:49:27 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // Loading page
 document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        var loader = document.getElementById("preloader");
-        loader.style.display = "none";
-        document.getElementById('video').style.display = 'block';
-    }, 3000); 
+  setTimeout(function() {
+      var loader = document.getElementById("preloader");
+      loader.style.display = "none";
+      document.getElementById('video').style.display = 'block';
+  }, 3000); 
 });
 
 // Loading
 window.addEventListener("load", function() {
-    setTimeout(function() {
-        var loader = document.getElementById("preloader");
-        loader.style.display = "none";
-    }, 4000); 
-    
+  setTimeout(function() {
+      var loader = document.getElementById("preloader");
+      loader.style.display = "none";
+  }, 4000); 
+  
 });
 
 // Cursor
@@ -21,84 +21,84 @@ var crsr = document.querySelector("#cursor");
 var blur = document.querySelector("#cursor-blur");
 
 document.addEventListener("mousemove", function(dets) {
-    crsr.style.left = dets.x+"px"
-    crsr.style.top = dets.y+"px"
-    blur.style.left = dets.x - 250 + "px"
-    blur.style.top = dets.y - 250 + "px"
+  crsr.style.left = dets.x+"px"
+  crsr.style.top = dets.y+"px"
+  blur.style.left = dets.x - 250 + "px"
+  blur.style.top = dets.y - 250 + "px"
 });
 
 // Toggle Button
 document.addEventListener('DOMContentLoaded', function() {
-  const toggleBtn = document.getElementById('toggle-btn');
-  const menuList = document.getElementById('menu-list');
-  const navClose = document.getElementById('nav-close');
+const toggleBtn = document.getElementById('toggle-btn');
+const menuList = document.getElementById('menu-list');
+const navClose = document.getElementById('nav-close');
 
-  toggleBtn.addEventListener('click', function() {
-      menuList.classList.toggle('show-menu');
-      if (menuList.classList.contains('show-menu')) {
-          menuList.style.display = "block";
-          navClose.classList.add("nav-close");
-          navClose.style.display = "block";
-          toggleBtn.style.display = "none";
-      }
-  });
+toggleBtn.addEventListener('click', function() {
+    menuList.classList.toggle('show-menu');
+    if (menuList.classList.contains('show-menu')) {
+        menuList.style.display = "block";
+        navClose.classList.add("nav-close");
+        navClose.style.display = "block";
+        toggleBtn.style.display = "none";
+    }
+});
 
-  navClose.addEventListener('click', function() {
-    menuList.classList.remove('show-menu');
-    menuList.style.display = "none";
-    navClose.classList.remove("nav-close");
-    navClose.style.display = "none";
-    toggleBtn.classList.add("toggle-btn");
-    toggleBtn.style.display = "block";
+navClose.addEventListener('click', function() {
+  menuList.classList.remove('show-menu');
+  menuList.style.display = "none";
+  navClose.classList.remove("nav-close");
+  navClose.style.display = "none";
+  toggleBtn.classList.add("toggle-btn");
+  toggleBtn.style.display = "block";
 });
 
 
 
-    const menuBtn = document.getElementById("mobile-menu-btn");
-    const mobileMenu = document.getElementById("mobile-menu-list");
-    const menuClose = document.getElementById("mobile-nav-close");
+  const menuBtn = document.getElementById("mobile-menu-btn");
+  const mobileMenu = document.getElementById("mobile-menu-list");
+  const menuClose = document.getElementById("mobile-nav-close");
 
-    menuBtn.addEventListener('click', function() {
-        mobileMenu.classList.toggle('show-mobile-menu');
-        if (mobileMenu.classList.contains('show-mobile-menu')) {
-            mobileMenu.style.display = "block";
-            menuClose.classList.add("nav-close");
-            menuClose.style.display = "block";
-            menuBtn.style.display = "none";
-        }
-        else {
-          mobileMenu.style.display = "none";
-          menuClose.classList.remove("nav-close");
-          menuClose.style.display = "none";
-          menuBtn.style.display = "block";
+  menuBtn.addEventListener('click', function() {
+      mobileMenu.classList.toggle('show-mobile-menu');
+      if (mobileMenu.classList.contains('show-mobile-menu')) {
+          mobileMenu.style.display = "block";
+          menuClose.classList.add("nav-close");
+          menuClose.style.display = "block";
+          menuBtn.style.display = "none";
       }
-    })
-    menuClose.addEventListener('click', function() {
-      mobileMenu.classList.remove('show-menu');
-      mobileMenu.style.display = "none";
-      menuClose.classList.remove("nav-close");
-      menuClose.style.display = "none";
-      menuBtn.classList.add("toggle-btn");
-      menuBtn.style.display = "block";
-  });
+      else {
+        mobileMenu.style.display = "none";
+        menuClose.classList.remove("nav-close");
+        menuClose.style.display = "none";
+        menuBtn.style.display = "block";
+    }
+  })
+  menuClose.addEventListener('click', function() {
+    mobileMenu.classList.remove('show-menu');
+    mobileMenu.style.display = "none";
+    menuClose.classList.remove("nav-close");
+    menuClose.style.display = "none";
+    menuBtn.classList.add("toggle-btn");
+    menuBtn.style.display = "block";
+});
 });
 
 // Function to start the video after a delay
 function startVideo() {
-    const video = document.getElementById('logo-video');
+  const video = document.getElementById('logo-video');
 
-    video.playbackRate = 0.75;
+  video.playbackRate = 0.75;
 
-    video.play()
-        .catch(error => {
-            console.error('Autoplay prevented:', error.message);
-        });
+  video.play()
+      .catch(error => {
+          console.error('Autoplay prevented:', error.message);
+      });
 }
 
 setTimeout(startVideo, 4000);
 
 // Scroll
-var container = document.getElementById('container');
+var container = document.getElementById('scroll-container');
 
 window.addEventListener('wheel', function(e) {
   if (e.deltaY > 0) {
@@ -114,30 +114,20 @@ window.addEventListener('wheel', function(e) {
   }
 });
 
-var container = document.getElementById('container');
-
-window.addEventListener('keydown', function(e) {
-  if (e.key === 'ArrowRight') {
-    container.scrollBy({
-      left: 100,
-      behavior: 'smooth'
-    });
-  } else if (e.key === 'ArrowLeft') {
-    container.scrollBy({
-      left: -100,
-      behavior: 'smooth'
-    });
-  } else if (e.key === 'ArrowUp') {
-    container.scrollBy({
-      top: 100,
-      behavior: 'smooth'
-    });
-  } else if (e.key === 'ArrowDown') {
-    container.scrollBy({
-      top: -100,
-      behavior: 'smooth'
-    });
+document.getElementById('scroll-container').addEventListener('wheel', function(event) {
+  if (event.deltaY !== 0) {
+    this.scrollLeft += event.deltaY;
+    event.preventDefault();
   }
 });
 
 
+
+window.onload = function() {
+	init();
+};
+
+function init() {
+	var menu = document.getElementById("menu");
+	menu.classList.add("transition-after-pageload");
+}
---
+++ js/test.js
@@ -0,0 +1,137 @@
+// Loading page
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var loader = document.getElementById("preloader");
+        loader.style.display = "none";
+        document.getElementById('video').style.display = 'block';
+    }, 3000); 
+});
+
+// Loading
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        var loader = document.getElementById("preloader");
+        loader.style.display = "none";
+    }, 4000); 
+    
+});
+
+// Cursor
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets) {
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 250 + "px"
+    blur.style.top = dets.y - 250 + "px"
+});
+
+// Toggle Button
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleBtn = document.getElementById('toggle-btn');
+  const menuList = document.getElementById('menu-list');
+  const navClose = document.getElementById('nav-close');
+
+  toggleBtn.addEventListener('click', function() {
+      menuList.classList.toggle('show-menu');
+      if (menuList.classList.contains('show-menu')) {
+          menuList.style.display = "block";
+          navClose.classList.add("nav-close");
+          navClose.style.display = "block";
+          toggleBtn.style.display = "none";
+      }
+  });
+
+  navClose.addEventListener('click', function() {
+    menuList.classList.remove('show-menu');
+    menuList.style.display = "none";
+    navClose.classList.remove("nav-close");
+    navClose.style.display = "none";
+    toggleBtn.classList.add("toggle-btn");
+    toggleBtn.style.display = "block";
+});
+
+
+
+    const menuBtn = document.getElementById("mobile-menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu-list");
+    const menuClose = document.getElementById("mobile-nav-close");
+
+    menuBtn.addEventListener('click', function() {
+        mobileMenu.classList.toggle('show-mobile-menu');
+        if (mobileMenu.classList.contains('show-mobile-menu')) {
+            mobileMenu.style.display = "block";
+            menuClose.classList.add("nav-close");
+            menuClose.style.display = "block";
+            menuBtn.style.display = "none";
+        }
+        else {
+          mobileMenu.style.display = "none";
+          menuClose.classList.remove("nav-close");
+          menuClose.style.display = "none";
+          menuBtn.style.display = "block";
+      }
+    })
+    menuClose.addEventListener('click', function() {
+      mobileMenu.classList.remove('show-menu');
+      mobileMenu.style.display = "none";
+      menuClose.classList.remove("nav-close");
+      menuClose.style.display = "none";
+      menuBtn.classList.add("toggle-btn");
+      menuBtn.style.display = "block";
+  });
+});
+
+// Function to start the video after a delay
+function startVideo() {
+    const video = document.getElementById('logo-video');
+
+    video.playbackRate = 0.75;
+
+    video.play()
+        .catch(error => {
+            console.error('Autoplay prevented:', error.message);
+        });
+}
+
+setTimeout(startVideo, 4000);
+
+// Scroll
+var item = document.getElementsByTagName('MAIN')[0];
+
+window.addEventListener('wheel', function(e) {
+
+  if (e.deltaY > 0) item.scrollLeft += 200;
+  else item.scrollLeft -= 200;
+});
+
+
+
+// var container = document.getElementById('container');
+
+// window.addEventListener('keydown', function(e) {
+//   if (e.key === 'ArrowRight') {
+//     container.scrollBy({
+//       left: 100,
+//       behavior: 'smooth'
+//     });
+//   } else if (e.key === 'ArrowLeft') {
+//     container.scrollBy({
+//       left: -100,
+//       behavior: 'smooth'
+//     });
+//   } else if (e.key === 'ArrowUp') {
+//     container.scrollBy({
+//       top: 100,
+//       behavior: 'smooth'
+//     });
+//   } else if (e.key === 'ArrowDown') {
+//     container.scrollBy({
+//       top: -100,
+//       behavior: 'smooth'
+//     });
+//   }
+// });
+
+
